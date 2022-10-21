87cc8b203dc24503f7d58a921a72e7cfede22d95
kiwoonjung <kiwoon0627@gmail.com>
2022-10-21 00:40:40 -0700

    update sprint2

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,64 +1,121 @@
 const showTime = [{
-date: "Mon Sept 06 2021",
-venue: "Ronald Lane",
-location: "San Francisco, CA",
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
 },
 {
-date: "Tue Sept 21 2021",
-venue: "Pier 3 East",
-location: "San Francisco, CA",
+    date: "Tue Sept 21 2021",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA",
 },
 {
-date: "Fri Oct 15 2021",
-venue: "View Lounge",
-location: "San Francisco, CA",
+    date: "Fri Oct 15 2021",
+    venue: "View Lounge",
+    location: "San Francisco, CA",
 },
 {
-date: "Sat Nov 06 2021",
-venue: "Hyatt Agency",
-location: "San Francisco, CA",
+    date: "Sat Nov 06 2021",
+    venue: "Hyatt Agency",
+    location: "San Francisco, CA",
 },
 {
-date: "Fri Nov 26 2021",
-venue: "Moscow Center",
-location: "San Francisco, CA",
+    date: "Fri Nov 26 2021",
+    venue: "Moscow Center",
+    location: "San Francisco, CA",
 },
 {
-date: "Wed Dec 15 2021",
-venue: "Press Club",
-location: "San Francisco, CA",
+    date: "Wed Dec 15 2021",
+    venue: "Press Club",
+    location: "San Francisco, CA",
 }];
 
-const showTimeList = document.querySelector('.showtime');
+const showTimeListContainer = document.getElementById('showtime');
+const showMainContainer = document.createElement('div');
+showMainContainer.classList.add("show-container")
+const showText = document.createElement('h2');
+showText.classList.add("show-text");
+showText.innerText = "Shows";
+showMainContainer.appendChild(showText);
 
-function createShowTimes(){
-    showTimeList.innerText = '';
-    
-    for(let i = 0; i < showTime.length; i++){
-        const showTimeDateList = document.createElement('h5');
+
+const tabletInfoContainer = document.createElement('div')
+tabletInfoContainer.classList.add("tablet-info");
+showMainContainer.appendChild(tabletInfoContainer);
+
+const tableDate = document.createElement('h6');
+tableDate.classList.add("show-tablet");
+tableDate.innerText = "DATE";
+tabletInfoContainer.appendChild(tableDate);
+
+const tableVenue = document.createElement('h6');
+tableVenue.classList.add("show-tablet");
+tableVenue.innerText = "VENUE";
+tabletInfoContainer.appendChild(tableVenue);
+
+const tableLocation = document.createElement('h6');
+tableLocation.classList.add("show-tablet");
+tableLocation.innerText = "LOCATION";
+tabletInfoContainer.appendChild(tableLocation);
+
+
+
+
+const showTimeList = document.createElement('ul');
+const showTimeListItem = document.createElement('li');
+showTimeListItem.classList.add("showtime-container");
+
+showMainContainer.appendChild(showTimeList)
+showTimeListContainer.appendChild(showMainContainer);
+
+function createShowTimes() {
+    showTimeListItem.innerText = '';
+
+    for (let i = 0; i < showTime.length; i++) {
+        const showTimeListItem = document.createElement('li');
+        showTimeListItem.classList.add("showtime-container")
+        showTimeList.appendChild(showTimeListItem);
+
+        const showTimeDateList = document.createElement('h6');
+        showTimeDateList.classList.add("h6-mecury");
         showTimeDateList.innerText = "DATE";
 
         const showTimeDate = document.createElement('h3');
         showTimeDate.innerText = showTime[i].date;
 
-        const showTimeVenueList = document.createElement('h5');
-        showTimeVenueList .innerText = "VENUE";
+        const showTimeVenueList = document.createElement('h6');
+        showTimeVenueList.classList.add("h6-mecury");
+        showTimeVenueList.innerText = "VENUE";
 
         const showTimeVenue = document.createElement('h3');
         showTimeVenue.innerText = showTime[i].venue;
 
-        const showTimeLocationList = document.createElement('h5');
+        const showTimeLocationList = document.createElement('h6');
+        showTimeLocationList.classList.add("h6-mecury");
         showTimeLocationList.innerText = "LOCATION";
 
         const showTimeLocation = document.createElement('h3');
         showTimeLocation.innerText = showTime[i].location;
 
-        showTimeList.appendChild(showTimeDateList);
-        showTimeList.appendChild(showTimeDate );
-        showTimeList.appendChild(showTimeVenueList);
-        showTimeList.appendChild(showTimeVenue);
-        showTimeList.appendChild(showTimeLocationList);
-        showTimeList.appendChild(showTimeLocation);
+        const showTimeButton = document.createElement('a');
+        showTimeButton.classList.add("ticket-text")
+        const linkText = document.createTextNode("my title text");
+        showTimeButton.appendChild(linkText);
+        showTimeButton.href = "#";
+        showTimeButton.innerText = "BUY TICKET";
+
+        const showTimeDivider = document.createElement('hr');
+        showTimeDivider.classList.add("divider")
+
+
+
+        showTimeListItem.appendChild(showTimeDateList);
+        showTimeListItem.appendChild(showTimeDate);
+        showTimeListItem.appendChild(showTimeVenueList);
+        showTimeListItem.appendChild(showTimeVenue);
+        showTimeListItem.appendChild(showTimeLocationList);
+        showTimeListItem.appendChild(showTimeLocation);
+        showTimeListItem.appendChild(showTimeButton);
+        showTimeListItem.appendChild(showTimeDivider);
     }
 }
 
